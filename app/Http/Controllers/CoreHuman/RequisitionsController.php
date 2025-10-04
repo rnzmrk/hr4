9220168382d4610a77e641121e5ee8a001dafbfc
rnzmrk <olearenzemark@gmail.com>
2025-10-05 01:43:57 +0800

    3

--- app/Http/Controllers/CoreHuman/RequisitionsController.php
+++ app/Http/Controllers/CoreHuman/RequisitionsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\CoreHuman;
 use App\Http\Controllers\Controller;
 use App\Models\Requisition;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class RequisitionsController extends Controller
 {
